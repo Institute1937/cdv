aeac2d14b3710ceb1b616744da6c216d221029b8
cerico <garethrobertlee@gmail.com>
2017-08-01 23:33:47 +0100

    fetch from clojure endpoint

--- client/js/index.js
+++ client/js/index.js
@@ -1,11 +1,19 @@
-import * as d3 from 'd3v4'; 
-
 document.body.onload = addElement;
 
 function addElement () { 
   let currentDiv = document.getElementById("front"); 
   var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode("Hello from js"); 
+  var newContent = document.createTextNode("I'm from js"); 
   newDiv.appendChild(newContent); //add the text node to the newly created div. 
   currentDiv.appendChild(newDiv)
+
+  fetch('http://localhost/clojure').then(function(res){
+    res.json().then(body => {
+      console.log(body)
+       var newDiv = document.createElement("div"); 
+       var newContent = document.createTextNode(body.message)
+       newDiv.appendChild(newContent); //add the text node to the newly created div. 
+       currentDiv.appendChild(newDiv)
+    });
+})
 }
